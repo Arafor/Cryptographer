20c38e61cde0073da66a57a0e0b0ea2f3decbdae
Arafor <zeroartis@hotmail.com>
2017-02-09 16:21:57 +0200

    Added Caesar cipher

--- Cryptographer/frmCaezar.Designer.cs
+++ Cryptographer/frmCaezar.Designer.cs
@@ -28,83 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtPlaintext = new System.Windows.Forms.TextBox();
-            this.txtKey = new System.Windows.Forms.TextBox();
-            this.txtCiphertext = new System.Windows.Forms.TextBox();
-            this.lblPlaintext = new System.Windows.Forms.Label();
-            this.lblKey = new System.Windows.Forms.Label();
-            this.lblCiphertext = new System.Windows.Forms.Label();
-            this.btnEncrypt = new System.Windows.Forms.Button();
             this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.lblCiphertext = new System.Windows.Forms.Label();
+            this.lblKey = new System.Windows.Forms.Label();
+            this.lblPlaintext = new System.Windows.Forms.Label();
+            this.txtCiphertext = new System.Windows.Forms.TextBox();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.txtPlaintext = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // txtPlaintext
+            // btnDecrypt
             // 
-            this.txtPlaintext.Location = new System.Drawing.Point(12, 25);
-            this.txtPlaintext.Name = "txtPlaintext";
-            this.txtPlaintext.Size = new System.Drawing.Size(100, 20);
-            this.txtPlaintext.TabIndex = 0;
-            // 
-            // txtKey
-            // 
-            this.txtKey.Location = new System.Drawing.Point(151, 25);
-            this.txtKey.Name = "txtKey";
-            this.txtKey.Size = new System.Drawing.Size(100, 20);
-            this.txtKey.TabIndex = 1;
-            // 
-            // txtCiphertext
-            // 
-            this.txtCiphertext.Location = new System.Drawing.Point(296, 25);
-            this.txtCiphertext.Name = "txtCiphertext";
-            this.txtCiphertext.Size = new System.Drawing.Size(100, 20);
-            this.txtCiphertext.TabIndex = 2;
-            // 
-            // lblPlaintext
-            // 
-            this.lblPlaintext.AutoSize = true;
-            this.lblPlaintext.Location = new System.Drawing.Point(12, 9);
-            this.lblPlaintext.Name = "lblPlaintext";
-            this.lblPlaintext.Size = new System.Drawing.Size(47, 13);
-            this.lblPlaintext.TabIndex = 3;
-            this.lblPlaintext.Text = "Plaintext";
-            // 
-            // lblKey
-            // 
-            this.lblKey.AutoSize = true;
-            this.lblKey.Location = new System.Drawing.Point(151, 9);
-            this.lblKey.Name = "lblKey";
-            this.lblKey.Size = new System.Drawing.Size(25, 13);
-            this.lblKey.TabIndex = 4;
-            this.lblKey.Text = "Key";
-            // 
-            // lblCiphertext
-            // 
-            this.lblCiphertext.AutoSize = true;
-            this.lblCiphertext.Location = new System.Drawing.Point(305, 9);
-            this.lblCiphertext.Name = "lblCiphertext";
-            this.lblCiphertext.Size = new System.Drawing.Size(54, 13);
-            this.lblCiphertext.TabIndex = 5;
-            this.lblCiphertext.Text = "Ciphertext";
+            this.btnDecrypt.Location = new System.Drawing.Point(463, 70);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 23;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
             // 
             // btnEncrypt
             // 
-            this.btnEncrypt.Location = new System.Drawing.Point(79, 88);
+            this.btnEncrypt.Location = new System.Drawing.Point(249, 70);
             this.btnEncrypt.Name = "btnEncrypt";
             this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
-            this.btnEncrypt.TabIndex = 6;
+            this.btnEncrypt.TabIndex = 22;
             this.btnEncrypt.Text = "Encrypt";
             this.btnEncrypt.UseVisualStyleBackColor = true;
             this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
             // 
-            // btnDecrypt
+            // lblCiphertext
             // 
-            this.btnDecrypt.Location = new System.Drawing.Point(244, 88);
-            this.btnDecrypt.Name = "btnDecrypt";
-            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
-            this.btnDecrypt.TabIndex = 7;
-            this.btnDecrypt.Text = "Decrypt";
-            this.btnDecrypt.UseVisualStyleBackColor = true;
-            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            this.lblCiphertext.AutoSize = true;
+            this.lblCiphertext.Location = new System.Drawing.Point(564, 10);
+            this.lblCiphertext.Name = "lblCiphertext";
+            this.lblCiphertext.Size = new System.Drawing.Size(54, 13);
+            this.lblCiphertext.TabIndex = 21;
+            this.lblCiphertext.Text = "Ciphertext";
+            // 
+            // lblKey
+            // 
+            this.lblKey.AutoSize = true;
+            this.lblKey.Location = new System.Drawing.Point(275, 10);
+            this.lblKey.Name = "lblKey";
+            this.lblKey.Size = new System.Drawing.Size(25, 13);
+            this.lblKey.TabIndex = 20;
+            this.lblKey.Text = "Key";
+            // 
+            // lblPlaintext
+            // 
+            this.lblPlaintext.AutoSize = true;
+            this.lblPlaintext.Location = new System.Drawing.Point(9, 10);
+            this.lblPlaintext.Name = "lblPlaintext";
+            this.lblPlaintext.Size = new System.Drawing.Size(47, 13);
+            this.lblPlaintext.TabIndex = 19;
+            this.lblPlaintext.Text = "Plaintext";
+            // 
+            // txtCiphertext
+            // 
+            this.txtCiphertext.Location = new System.Drawing.Point(555, 26);
+            this.txtCiphertext.Multiline = true;
+            this.txtCiphertext.Name = "txtCiphertext";
+            this.txtCiphertext.Size = new System.Drawing.Size(220, 525);
+            this.txtCiphertext.TabIndex = 18;
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(265, 26);
+            this.txtKey.Multiline = true;
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(250, 20);
+            this.txtKey.TabIndex = 17;
+            // 
+            // txtPlaintext
+            // 
+            this.txtPlaintext.Location = new System.Drawing.Point(9, 26);
+            this.txtPlaintext.Multiline = true;
+            this.txtPlaintext.Name = "txtPlaintext";
+            this.txtPlaintext.Size = new System.Drawing.Size(220, 525);
+            this.txtPlaintext.TabIndex = 16;
             // 
             // frmCaezar
             // 
@@ -120,7 +123,7 @@
             this.Controls.Add(this.txtKey);
             this.Controls.Add(this.txtPlaintext);
             this.Name = "frmCaezar";
-            this.Text = "Cryptographer- Cēzara šifrs";
+            this.Text = "Cryptographer- Caesar cipher";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -128,13 +131,13 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox txtPlaintext;
-        private System.Windows.Forms.TextBox txtKey;
-        private System.Windows.Forms.TextBox txtCiphertext;
-        private System.Windows.Forms.Label lblPlaintext;
-        private System.Windows.Forms.Label lblKey;
-        private System.Windows.Forms.Label lblCiphertext;
-        private System.Windows.Forms.Button btnEncrypt;
         private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Label lblCiphertext;
+        private System.Windows.Forms.Label lblKey;
+        private System.Windows.Forms.Label lblPlaintext;
+        private System.Windows.Forms.TextBox txtCiphertext;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.TextBox txtPlaintext;
     }
 }
--- Cryptographer/frmCaezar.cs
+++ Cryptographer/frmCaezar.cs
@@ -17,20 +17,117 @@ namespace Cryptographer
             InitializeComponent();
         }
 
-        /*public encrypt(string plaintext, int key)
+        NumericalAlphabet numAlphabet = new NumericalAlphabet();
+
+        public Boolean checkEmptyFields()
         {
-            string ciphertext;
-            return ciphertext;
-        }*/
+            // Check if there is a message to encrypt
+            if (txtPlaintext.Text.Length > 0 || txtCiphertext.Text.Length > 0)
+            {
+                // Check if there is a key specified
+                if (txtKey.Text.Length > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show("Atslēga nedrīkst būt tukša!");
+                    return false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lūdzu ievadiet ziņu!");
+                return false;
+            }
+        }
+
+        public string cipher(int[] messageInInt, int key, string function)
+        {
+            // Encrypt message using key
+            for (int i = 0; i < messageInInt.Length; i++)
+            {
+                if (function == "E")
+                {
+                    messageInInt[i] = messageInInt[i] + key;
+                }
+                else if (function == "D")
+                {
+                    messageInInt[i] = messageInInt[i] - key;
+                }
+                else
+                {
+                    MessageBox.Show("Neatpazīta funkcija!");
+                    break;
+                }
+            }
+
+            // Get the string value of our result message
+            char[] messageInChar = numAlphabet.numbersToLetters(messageInInt);
+            string messageInString = "";
+            for (int i = 0; i < messageInChar.Length; i++)
+            {
+                messageInString = messageInString + messageInChar[i].ToString();
+            }
+            return messageInString;
+        }
 
         private void btnEncrypt_Click(object sender, EventArgs e)
         {
-            //encrypt(txtPlaintext.Text, int.Parse(txtKey.Text));
+            // Prepare for cipher text display
+            txtCiphertext.Text = "";
+            if (checkEmptyFields())
+            {
+                // Get plaintext and key
+                char[] plaintextArray = txtPlaintext.Text.ToUpper().ToCharArray();
+                int key = 0;
+                try
+                {
+                    key = int.Parse(txtKey.Text);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Lūdzu norādīet atslēgu kā naturālu skaitli!");
+                    txtKey.Text = "";
+                }
+
+                // Translate message to numerical values
+                int[] message = numAlphabet.lettersToNumbers(plaintextArray);
+                if (txtKey.Text != "")
+                {
+                    if (message != null)
+                    {
+                        txtCiphertext.Text = cipher(message, key, "E");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Lūdzu izmantojiet tekstā tikai latīņu burtus no A līdz Z!");
+                    }
+                }
+            }
         }
 
         private void btnDecrypt_Click(object sender, EventArgs e)
         {
-            //encrypt(txtCiphertext.Text, int.Parse(txtKey.Text) * -1);
+            // Prepare for cipher text display
+            txtPlaintext.Text = "";
+            if (checkEmptyFields())
+            {
+                // Get plaintext and key
+                char[] ciphertextArray = txtCiphertext.Text.ToUpper().ToCharArray();
+                int key = int.Parse(txtKey.Text);
+
+                // Translate message to numerical values
+                int[] message = numAlphabet.lettersToNumbers(ciphertextArray);
+                if (message != null && key != null)
+                {
+                    txtPlaintext.Text = cipher(message, key, "D");
+                }
+                else
+                {
+                    MessageBox.Show("Lūdzu izmantojiet tekstā tikai latīņu burtus no A līdz Z!");
+                }
+            }
         }
     }
 }
