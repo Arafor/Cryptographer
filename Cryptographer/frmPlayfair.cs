82e80e0f573ff5b7b4b45c3dd54c1aeb8981ec16
Arafor <zeroartis@hotmail.com>
2017-02-09 18:23:44 +0200

    Added arrays for allowed letters, replaceable letters and basis of table array

--- Cryptographer/frmPlayfair.cs
+++ Cryptographer/frmPlayfair.cs
@@ -12,6 +12,61 @@ namespace Cryptographer
 {
     public partial class frmPlayfair : Form
     {
+        char[] allowedLetters =
+        {
+                Convert.ToChar("A"),
+                Convert.ToChar("B"),
+                Convert.ToChar("C"),
+                Convert.ToChar("D"),
+                Convert.ToChar("E"),
+                Convert.ToChar("F"),
+                Convert.ToChar("G"),
+                Convert.ToChar("H"),
+                Convert.ToChar("I"),
+                Convert.ToChar("J"),
+                Convert.ToChar("K"),
+                Convert.ToChar("L"),
+                Convert.ToChar("M"),
+                Convert.ToChar("N"),
+                Convert.ToChar("O"),
+                Convert.ToChar("P"),
+                Convert.ToChar("Q"),
+                Convert.ToChar("R"),
+                Convert.ToChar("S"),
+                Convert.ToChar("T"),
+                Convert.ToChar("U"),
+                Convert.ToChar("V"),
+                Convert.ToChar("W"),
+                Convert.ToChar("X"),
+                Convert.ToChar("Y"),
+                Convert.ToChar("Z")
+        };
+
+        char[] replaceableLetters =
+        {
+                Convert.ToChar(" "),
+                Convert.ToChar(","),
+                Convert.ToChar("."),
+                Convert.ToChar("?"),
+                Convert.ToChar("!"),
+        };
+        char[,] table = new char[5, 5]
+        {
+            { },
+            { },
+            { },
+            { },
+            { },
+
+        };
+
+        public char[] parseText(char[] message)
+        {
+            char[] parsedMessage;
+
+            return parsedMessage;
+        }
+
         public frmPlayfair()
         {
             InitializeComponent();
@@ -25,7 +80,8 @@ namespace Cryptographer
 
         private void btnCipher_Click(object sender, EventArgs e)
         {
-
+            char[] message = txtMessage.Text.ToCharArray();
+            
         }
     }
 }
