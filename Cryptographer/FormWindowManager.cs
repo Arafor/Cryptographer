3ed4b4fc7e146841e08e65174b37d249dbc9a642
Arafor <zeroartis@hotmail.com>
2017-04-25 14:54:59 +0300

    fixed form windows being behind the task bar. Fixed info form window being infront of cryptographer

--- Cryptographer/FormWindowManager.cs
+++ Cryptographer/FormWindowManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,13 +11,67 @@ namespace Cryptographer
 {
     class FormWindowManager
     {
-        // Get screen resolution
+        // Get working area of primary screen (no task bar)
         static Rectangle resolution = Screen.PrimaryScreen.Bounds;
+        Rectangle taskbar = new Rectangle();
+        //Size(Screen.PrimaryScreen.Bounds.Width - Screen.PrimaryScreen.WorkingArea.Width, Screen.PrimaryScreen.Bounds.Height - Screen.PrimaryScreen.WorkingArea.Height);
         int resolutionWidth = resolution.Width;
         int resolutionHeight = resolution.Height;
         // For testing on other resolutions
         //int resolutionWidth = 320;
         //int resolutionHeight = 200;
+
+        public FormWindowManager()
+        {
+            taskbar = GetTaskbarPosition();
+            if (resolution.Width != taskbar.Width)
+            {
+                resolutionWidth = resolution.Width - taskbar.Width;
+            }
+
+            if (resolution.Height != taskbar.Height)
+            {
+                resolutionHeight = resolution.Height - taskbar.Height;
+            }
+        }
+
+        public static Rectangle GetTaskbarPosition()
+        {
+            var data = new APPBARDATA();
+            data.cbSize = System.Runtime.InteropServices.Marshal.SizeOf(data);
+            IntPtr retval = SHAppBarMessage(ABM_GETTASKBARPOS, ref data);
+            if (retval == IntPtr.Zero) throw new Win32Exception("Please re-install Windows");
+            return new Rectangle(data.rc.left, data.rc.top,
+                data.rc.right - data.rc.left, data.rc.bottom - data.rc.top);
+
+        }
+
+        // P/Invoke goo:
+        private const int ABM_GETTASKBARPOS = 5;
+        [System.Runtime.InteropServices.DllImport("shell32.dll")]
+        private static extern IntPtr SHAppBarMessage(int msg, ref APPBARDATA data);
+        private struct APPBARDATA
+        {
+            public int cbSize;
+            public IntPtr hWnd;
+            public int uCallbackMessage;
+            public int uEdge;
+            public RECT rc;
+            public IntPtr lParam;
+        }
+        private struct RECT
+        {
+            public int left, top, right, bottom;
+        }
+
+        public Rectangle getTaskbar()
+        {
+            Rectangle taskbar = new Rectangle();
+            taskbar.Size = new Size(Screen.PrimaryScreen.Bounds.Width - Screen.PrimaryScreen.WorkingArea.Width, Screen.PrimaryScreen.Bounds.Height - Screen.PrimaryScreen.WorkingArea.Height);
+            taskbar.Location = new Point(Screen.PrimaryScreen.WorkingArea.Location.X, Screen.PrimaryScreen.WorkingArea.Location.Y);
+
+            return taskbar;
+        }
 
         public void setFormWindowLocation(Form previousForm, Form targetForm)
         {
