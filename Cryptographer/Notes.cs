d51cceba5a4bfb9a39999321710a974dfb1173be
Arafor <zeroartis@hotmail.com>
2017-04-26 14:31:33 +0300

    Added noted to keep old unused code that could be useful later on

---
+++ Cryptographer/Notes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptographer
+{
+    class Notes
+    {
+        /* Tooltip box ----------------------------------------------------------- */
+        //Global
+        //ToolTip textboxToolTip = new ToolTip();
+
+        //On load
+        /*textboxToolTip.IsBalloon = true;
+        textboxToolTip.AutoPopDelay = 1000;
+        textboxToolTip.InitialDelay = 1000;
+        textboxToolTip.ReshowDelay = 500;*/
+
+        //In code
+        //textboxToolTip.Hide(txtPrimeNumber);
+        //textboxToolTip.Show(string.Empty, txtPrimeNumber);
+        //textboxToolTip.Show("The entered value is not a prime number", txtPrimeNumber);
+    }
+}
